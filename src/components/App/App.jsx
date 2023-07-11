ad6f88736a01247bd01c61a1f92e3e97faa87df9
Maria Kushnareva <kushnareva.msee@gmail.com>
2023-07-11 11:44:47 +0300

    feat: сверстать промо

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from '../Header/Header.jsx';
+import Main from '../Main/Main.jsx';
 
 function App() {
   return (
     <div className="page">
       <Header />
-
+      <Main>
+      </Main>
     </div>
   );
 }
