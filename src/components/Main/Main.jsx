8d05b5d00bc174310dc43fac1c668028064bb98e
Maria Kushnareva <kushnareva.msee@gmail.com>
2023-07-12 12:26:47 +0300

    refactor: сделать перестановку в директориях

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import Promo from './Promo/Promo.jsx';
+import Promo from '../Promo/Promo.jsx';
+import NavTab from '../NavTab/NavTab.jsx';
 
 function Main() {
   return (
     <main className='content'>
       <Promo />
+      <NavTab />
     </main>
   );
 }
