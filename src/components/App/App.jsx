a6d640cf8d153ed98bf3329c991ac31740ca2432
Maria Kushnareva <kushnareva.msee@gmail.com>
2023-08-04 16:05:17 +0300

    feat: создать защищённые роуты

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { Routes, Route } from "react-router-dom";
-import CurrentUserContext from "../../contexts/CurrentUserContext.js"
+import { Routes, Route, useLocation } from "react-router-dom";
+import CurrentUserContext from "../../contexts/CurrentUserContext.js";
 import Header from "../Header/Header.jsx";
 import Main from "../Main/Main.jsx";
 import Footer from "../Footer/Footer.jsx";
@@ -12,11 +12,16 @@ import PageNotFound from "../PageNotFound/PageNotFound.jsx";
 import Profile from "../Profile/Profile.jsx";
 // import Preloader from "../UIComponents/Preloader/Preloader.jsx"
 import { getMovies, MOVIES_PATH } from "../../utils/MoviesApi.js";
+import ProtectedRoute from "../ProtectedRoute/ProtectedRoute.jsx";
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(true);
   const [allMovies, setAllMovies] = useState([]);
   const [currentUser, setCurrentUser] = useState({});
+
+  const location = useLocation().pathname;
+  const locationWithHeader = ["/", "/movies", "/saved-movies", "/profile"];
+  const locationWithFooter = ["/", "/movies", "/saved-movies"];
 
   function logout() {
     setLoggedIn(false);
@@ -46,84 +51,45 @@ function App() {
       .catch((err) => {
         console.log(err);
         setAllMovies([]);
-      })
+      });
   };
   getMoviesArray();
 
-    // // сохряняю все фильмы в стейт
-    // const saveAllMovies = () => {
-    //   if (allMovies < 1) {
-    //     const localStorageMovies = JSON.parse(localStorage.getItem('movies'));
-    //     if (!localStorageMovies) {
-    //       getMoviesArray();
-    //     } else {
-    //       getMoviesArray(localStorageMovies);
-    //     }
-    //   }
-    // };
+  // // сохряняю все фильмы в стейт
+  // const saveAllMovies = () => {
+  //   if (allMovies < 1) {
+  //     const localStorageMovies = JSON.parse(localStorage.getItem('movies'));
+  //     if (!localStorageMovies) {
+  //       getMoviesArray();
+  //     } else {
+  //       getMoviesArray(localStorageMovies);
+  //     }
+  //   }
+  // };
 
   return (
     <CurrentUserContext.Provider value={currentUser}>
       <div className="page">
+        {locationWithHeader.includes(location) && <Header loggedIn={loggedIn} />}
         <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Header loggedIn={loggedIn} />
-                <Main />
-                <Footer />
-              </>
-            }
-          />
+          <Route path="/" element={<Main />} />
           <Route
             path="/movies"
-            element={
-              <>
-                <Header loggedIn={loggedIn} />
-                <Movies allMovies={allMovies}/>
-                <Footer />
-              </>
-            }
+            element={<ProtectedRoute element={Movies} allMovies={allMovies} />}
           />
           <Route
             path="/saved-movies"
-            element={
-              <>
-                <Header loggedIn={loggedIn} />
-                <SavedMovies />
-                <Footer />
-              </>
-            }
+            element={<ProtectedRoute element={SavedMovies} />}
           />
           <Route
             path="/profile"
-            element={
-              <>
-                <Header loggedIn={loggedIn} />
-                <Profile logout={logout} />
-              </>
-            }
+            element={<ProtectedRoute element={Profile} logout={logout} />}
           />
-          <Route
-            path="/signup"
-            element={
-              <Register loggedIn={loggedIn} />
-            }
-          />
-          <Route
-            path="/signin"
-            element={
-              <Login />
-            }
-          />
-          <Route
-            exact path='/*'
-            element={
-              <PageNotFound />
-            }
-          />
+          <Route path="/signup" element={<Register loggedIn={loggedIn} />} />
+          <Route path="/signin" element={<Login />} />
+          <Route exact path="/*" element={<PageNotFound />} />
         </Routes>
+        {locationWithFooter.includes(location) && <Footer />}
       </div>
     </CurrentUserContext.Provider>
   );
